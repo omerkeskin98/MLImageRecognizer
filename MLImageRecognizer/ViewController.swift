3779449bb7cc3e0d2c184f34b3aa55482b5c9f0b
Omer Faruk Keskin <keskinomerf@gmail.com>
2024-04-13 21:56:14 +0300

    image upload with camera added

--- MLImageRecognizer/ViewController.swift
+++ MLImageRecognizer/ViewController.swift
@@ -6,14 +6,109 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageLabel: UILabel!
+    var chosenImage = CIImage()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    @IBAction func uploadClicked(_ sender: Any) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        //  imagePicker.sourceType = .photoLibrary
+        //present(imagePicker, animated: false, completion: nil)
+        
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        // Option to take a photo
+        let takePhotoAction = UIAlertAction(title: "Take Photo", style: .default) { _ in
+            imagePicker.sourceType = .camera
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        actionSheet.addAction(takePhotoAction)
+        
+        // Option to choose from library
+        let chooseFromLibraryAction = UIAlertAction(title: "Choose from Library", style: .default) { _ in
+            imagePicker.sourceType = .photoLibrary
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        actionSheet.addAction(chooseFromLibraryAction)
+        
+        // Cancel action
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        actionSheet.addAction(cancelAction)
+        
+        present(actionSheet, animated: true, completion: nil)
+    }
 
-
+        
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: false, completion: nil)
+        
+        if let ciImage = CIImage(image: imageView.image!){
+            chosenImage = ciImage
+            
+        }
+        
+        recognizeImage(image: chosenImage)
+    }
+    
+    func recognizeImage(image: CIImage){
+        
+        if let model = try? VNCoreMLModel(for: MobileNetV2().model) {
+            let request = VNCoreMLRequest(model: model) { (vnrequest, error) in
+                
+                if let results = vnrequest.results as? [VNClassificationObservation] {
+                    if results.count > 0 {
+                        
+                        let topResult = results.first
+                        
+                        DispatchQueue.main.async {
+                            
+                            let confidenceLevel = (topResult?.confidence ?? 0) * 100
+                            
+                            let rounded = Int (confidenceLevel * 100) / 100
+                            
+                            self.imageLabel.text = "\(rounded)% it's a(n) \(topResult!.identifier)"
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            let handler = VNImageRequestHandler(ciImage: image)
+                  DispatchQueue.global(qos: .userInteractive).async {
+                    do {
+                    try handler.perform([request])
+                    } catch {
+                        print("error")
+                    }
+            }
+            
+            
+        }
+        
+    }
+    
 }
 
